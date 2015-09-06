3c93cb34e9826a152786a5d1d9f32c8824ce58f0
周迁 <zhouqian1103@gmail.com>
2015-09-06 15:43:55 +0800

    add requirements

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from setuptools import setup, find_packages
 
 version = '0.0.1'
@@ -5,9 +7,10 @@ version = '0.0.1'
 setup(
     name='tornmem',
     version=version,
-    description="async memcached client base on tornado io-loop",
+    description="Asynchronous Memcached client that works within Tornado IO loop",
     packages=['tornmem'],
     author="Eddy Zhou",
     author_email="zhouqian1103@gmail.com",
+    license="http://www.apache.org/licenses/LICENSE-2.0",
     url="https://github.com/eddyzhou/tornado-memcached",
 )
