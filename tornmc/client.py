bf14152dd4fa852c61b25f42ada0f2eb5195a3e2
周迁 <zhouqian1103@gmail.com>
2016-01-26 15:15:28 +0800

    check key

--- tornmc/client.py
+++ tornmc/client.py
@@ -4,11 +4,14 @@
 import cPickle as pickle
 import collections
 import logging
+import re
 import zlib
 from binascii import crc32
 from cStringIO import StringIO
 
 from pool import Pool
+
+import six
 
 import tornado.gen
 import tornado.ioloop
@@ -19,6 +22,8 @@ def cmemcache_hash(key):
 
 server_hash_function = cmemcache_hash
 
+valid_key_chars_re = re.compile(b'[\x21-\x7e\x80-\xff]+$')
+
 _FLAG_PICKLE = 1 << 0
 _FLAG_INTEGER = 1 << 1
 _FLAG_LONG = 1 << 2
@@ -26,6 +31,10 @@ _FLAG_COMPRESSED = 1 << 3
 
 
 class MemcachedError(Exception):
+    pass
+
+
+class MemcachedKeyError(MemcachedError):
     pass
 
 
@@ -56,16 +65,19 @@ class Client:
 
     @tornado.gen.coroutine
     def get(self, key):
+        self._check_key(key)
         result = yield self._get('get', key)
         raise tornado.gen.Return(result)
 
     @tornado.gen.coroutine
     def gets(self, key):
+        self._check_key(key)
         result = yield self._get('gets', key)
         raise tornado.gen.Return(result)
 
     @tornado.gen.coroutine
     def cas(self, key, cas_id, value, expire=0, min_compress_len=0):
+        self._check_key(key)
         flags, value = self.get_store_info(value, min_compress_len)
         connection = yield self.get_connection(key=key)
         try:
@@ -112,6 +124,15 @@ class Client:
             response = result
         raise tornado.gen.Return(response)
 
+    def _check_key(self, key, key_prefix=b''):
+        if not isinstance(key, six.binary_type):
+            raise MemcachedKeyError('No ascii key: %s' % key)
+        key = key_prefix + key
+        if not valid_key_chars_re.match(key):
+            raise MemcachedKeyError('Key contains invalid character: %s' % key)
+        if len(key) > 250:
+            raise MemcachedKeyError('Key is too long: %s' % key)
+
     def _raise_errors(self, line, cmd):
         if line.startswith(b'ERROR'):
             raise MemcachedUnknownCommandError(cmd)
@@ -147,6 +168,9 @@ class Client:
 
     @tornado.gen.coroutine
     def get_multi(self, keys, key_prefix=''):
+        for key in keys:
+            self._check_key(key, key_prefix)
+
         response = {}
         orig_to_noprefix = dict((key_prefix+str(k), k) for k in keys)
         key_dict = self._group_keys(keys, key_prefix)
@@ -181,6 +205,9 @@ class Client:
 
     @tornado.gen.coroutine
     def set_multi(self, mapping, expire=0, key_prefix='', min_compress_len=0):
+        for k in mapping:
+            self._check_key(k, key_prefix)
+
         failed_list = []
         for k, value in mapping.iteritems():
             key = key_prefix + str(k)
@@ -204,17 +231,20 @@ class Client:
 
     @tornado.gen.coroutine
     def set(self, key, value, expire=0, min_compress_len=0):
+        self._check_key(key)
         result = yield self._set('set', key, value, expire, min_compress_len)
         raise tornado.gen.Return(result)
 
     @tornado.gen.coroutine
     def replace(self, key, value, expire=0, min_compress_len=0):
+        self._check_key(key)
         result = yield self._set('replace', key, value,
                                  expire, min_compress_len)
         raise tornado.gen.Return(result)
 
     @tornado.gen.coroutine
     def add(self, key, value, expire=0, min_compress_len=0):
+        self._check_key(key)
         result = yield self._set('add', key, value, expire, min_compress_len)
         raise tornado.gen.Return(result)
 
@@ -266,16 +296,19 @@ class Client:
 
     @tornado.gen.coroutine
     def incr(self, key, delta=1):
+        self._check_key(key)
         result = yield self._incr_or_decr('incr', key, delta)
         raise tornado.gen.Return(result)
 
     @tornado.gen.coroutine
     def decr(self, key, delta=1):
+        self._check_key(key)
         result = yield self._incr_or_decr('decr', key, delta)
         raise tornado.gen.Return(result)
 
     @tornado.gen.coroutine
     def delete(self, key):
+        self._check_key(key)
         connection = yield self.get_connection(key=key)
         try:
             cmd = 'delete %s' % (key)
